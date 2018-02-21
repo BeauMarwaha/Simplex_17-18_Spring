acb23c84531056f6b596e076e7e7830a7def9b96
Beau Marwaha <beaumarwaha@gmail.com>
2018-02-21 14:15:39 -0500

    A03 Deliverable
    A03 Deliverable

--- A03 - LERP/AppClass.cpp
+++ A03 - LERP/AppClass.cpp
@@ -2,7 +2,7 @@
 void Application::InitVariables(void)
 {
 	//Change this to your name and email
-	m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu";
+	m_sProgrammer = "Beau Marwaha - bcm2463@rit.edu";
 	
 	//Set the position and target of the camera
 	//(I'm at [0,0,10], looking at [0,0,0] and up is the positive Y axis)
@@ -36,6 +36,19 @@ void Application::InitVariables(void)
 	{
 		vector3 v3Color = WaveLengthToRGB(uColor); //calculate color based on wavelength
 		m_shapeList.push_back(m_pMeshMngr->GenerateTorus(fSize, fSize - 0.1f, 3, i, v3Color)); //generate a custom torus and add it to the meshmanager
+
+		//Generate the stops for this shape
+		std::vector<vector3> stops;
+		for (int x = 0; x < i; x++)
+		{
+			float angle = (PI * 2) * ((float)x / (float)i);
+			float sine = sin(angle);
+			float cosine = cos(angle);
+			stops.push_back(vector3(cosine * fSize, sine * fSize, 0));
+		}
+		m_lastStops.push_back(0); //add the starting point as the initial last stop for each shape
+		m_stopsList.push_back(stops); //add the generated stops to the stops list
+
 		fSize += 0.5f; //increment the size for the next orbit
 		uColor -= static_cast<uint>(decrements); //decrease the wavelength
 	}
@@ -62,15 +75,62 @@ void Application::Display(void)
 	/*
 		The following offset will orient the orbits as in the demo, start without it to make your life easier.
 	*/
-	//m4Offset = glm::rotate(IDENTITY_M4, 90.0f, AXIS_Z);
+	m4Offset = glm::rotate(IDENTITY_M4, 90.0f, AXIS_Z);
+
+
+	//Get a timer
+	static float fTimer = 0;	//store the new timer
+	static uint uClock = m_pSystem->GenClock(); //generate a new clock for that timer
+	fTimer += m_pSystem->GetDeltaTime(uClock); //get the delta time for that timer
+
+	static float distanceToNextStop = 0; // Static tracker of the percentage of the way to the next stop
+	static int lastFullSecond = 0; // Static tracker of the last full second of game run time
+
+	// Calculates the current time since the last full second 
+	distanceToNextStop = fmod(fTimer, 1);
+
+
+	// This logic allows us to figure out when the object should start moving to the next stop by incrementing lastStop every one second
+	//  as one second is how long it will take the object to traverse one stop
+	if (lastFullSecond < (int)fTimer)
+	{
+		// Set the last full second to the current second
+		lastFullSecond = (int)fTimer;
+
+		// Reset the distance to the next stop to 0
+		distanceToNextStop = 0;
+
+		// Increment the last stop for all shapes unless this will be the final stop in which case reset last stop to 0
+		for (int i = 0; i < m_uOrbits; i++)
+		{
+			if ((m_lastStops[i] + 1) != m_stopsList[i].size())
+			{
+				m_lastStops[i]++;
+			}
+			else
+			{
+				m_lastStops[i] = 0;
+			}
+		}
+	}
 
 	// draw a shapes
 	for (uint i = 0; i < m_uOrbits; ++i)
 	{
 		m_pMeshMngr->AddMeshToRenderList(m_shapeList[i], glm::rotate(m4Offset, 90.0f, AXIS_X));
 
-		//calculate the current position
 		vector3 v3CurrentPos = ZERO_V3;
+
+		// LERP logic between the concurrent points in the stops list 
+		//  unless this will be the final stop in which case LERP between the final and first stops
+		if ((m_lastStops[i] + 1) != m_stopsList[i].size())
+		{
+			v3CurrentPos = glm::lerp(m_stopsList[i][m_lastStops[i]], m_stopsList[i][m_lastStops[i] + 1], distanceToNextStop);
+		}
+		else
+		{
+			v3CurrentPos = glm::lerp(m_stopsList[i][m_stopsList[i].size() - 1], m_stopsList[i][0], distanceToNextStop);
+		}
 		matrix4 m4Model = glm::translate(m4Offset, v3CurrentPos);
 
 		//draw spheres
--- A03 - LERP/AppClass.h
+++ A03 - LERP/AppClass.h
@@ -20,7 +20,9 @@ class Application
 {
 	uint m_uOrbits = 0; //number of shapes starting at 3 and increasing in sides
 	std::vector<uint> m_shapeList; //shape index for circles
-	String m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu";
+	String m_sProgrammer = "Beau Marwaha - bcm2463@rit.edu";
+	std::vector<std::vector<vector3>> m_stopsList; //list of stops for the spheres around the shapes
+	std::vector<int> m_lastStops; //tracker of what the last stop was for each sphere
 	
 private:
 	static ImGuiObject gui; //GUI object
